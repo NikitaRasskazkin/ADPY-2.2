3c64190568fb8dc95a6b73a9a46af476a06622a9
NikitaRasskazkin <rasskazkin64@gmail.com>
2020-09-11 17:32:39 +0400

    init commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import hashlib
+
+
+def hash_generator(path: str = 'files/file.txt'):
+    with open(path, encoding='utf-8') as f:
+        for line in f:
+            hash_line = hashlib.md5(bytes(line.rstrip(), encoding='utf-8'))
+            yield hash_line.hexdigest()
+
+
+if __name__ == '__main__':
+    generator = hash_generator()
+    for item in generator:
+        print(item)
